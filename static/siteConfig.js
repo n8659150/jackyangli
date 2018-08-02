5103913a7e78324debbcaa93a7e96f9dc2fb5ca2
n8659150 <jackli900421@hotmail.com>
2018-08-02 20:35:14 +0800

    upload source code

---
+++ static/siteConfig.js
@@ -0,0 +1,61 @@
+export default {
+    // proloader
+    pageLoaded: false,
+    //HOME section
+    siteTitle: '校长不读书',
+    navItems: [
+        { id: 'home', name: '首页' },
+        { id: 'about', name: '关于' },
+        { id: 'work', name: '爱好' },
+        { id: 'contact', name: '联系' }
+    ],
+    profileImg: 'static/images/jackli-profile.jpg',
+    description: "不爱喝咖啡的吉他手不是好程序员 不爱弹吉他的程序员不是咖啡狂魔 不爱写代码的咖啡狂魔不是好主唱",
+    //ABOUT section
+    aboutTitle: '关于校长',
+    // aboutIntro: '',
+    aboutParas: '出国读研时获得的外号。国外签到什么的管得不严，尤其是计算机类学科，也不是靠每天每天去上课就真的能学会的。研究生后期就比较少去上课了(其实是在图书馆)，所以同学们都很少见到我，大概一到两周能见一次吧，和校长来学校的频率差不多，被叫着叫着就习惯了 = =',
+    images: [
+        { src: 'static/images/instagram-image1.jpg', link: 'https://www.instagram.com/p/cqED9yjN0j/' },
+        { src: 'static/images/instagram-image2.jpg', link: 'https://www.instagram.com/p/enL0SsDN51/' },
+        { src: 'static/images/instagram-image3.jpg', link: 'https://www.instagram.com/p/apOuRTDNwJ/' },
+        { src: 'static/images/instagram-image4.jpg', link: 'https://www.instagram.com/p/ZXIj33jN2v/' },
+        { src: 'static/images/instagram-image5.jpg', link: 'https://www.instagram.com/p/Xwev1FjN9-/' }
+    ],
+    //SKILL section
+    skillTitle: '手艺',
+    skillDesc: '前端还过得去，后端有一定程度地了解。前端的话，MVVM框架用得比较多，偏爱Vuejs；后端的话python用得比较多；服务端的话，nodejs也不是不能写。这人对代码格式有洁癖，请小心',
+    bar1Title: '前端',
+    bar1Width: '80%',
+    bar2Title: '后端',
+    bar2Width: '50%',
+    bar3Title: '修电脑',
+    bar3Width: '1%',
+    //WORK section
+    workTitle: "爱好",
+    workDesc: "写代码，弹吉他，写歌，喝咖啡",
+    thumb1Title: "前端开发了解一下",
+    thumb2Title: "词曲创作了解一下",
+    thumb3Title: "星座命理了解一下",
+    thumb4Title: "Github了解一下",
+    thumbs: [
+        { src: 'static/images/work-1.jpg', link: 'static/images/work-1.jpg' },
+        { src: 'static/images/work-2.jpg', link: 'static/images/work-2.jpg' },
+        { src: 'static/images/work-3.jpg', link: 'static/images/work-3.jpg' },
+        { src: 'static/images/work-4.jpg', link: 'static/images/work-4.jpg' }
+    ],
+    //CONTACT section
+    contactTitle: '想给我留言？',
+    contactDesc: '随时欢迎，看到了我会回复的！',
+    // field1Name: 'Name',
+    // field1Type: 'text',
+    // field2Name: 'Email',
+    // field2Type: 'email',
+    // field3Name: 'Subject',
+    // field3Type: 'text',
+    // textAreaFieldName: 'Message',
+    // buttonName: 'Send Message',
+    //FOOTER section
+    footerText: '2018 Jack Li© refactoring with Vue.js'
+
+}
